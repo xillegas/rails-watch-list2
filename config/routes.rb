c2e1c616c906a4f7b2e3b87baa49015c8899d47f
Alfredo Alejandro Villegas <xillegas@gmail.com>
2021-10-22 16:04:24 -0400

    bookmark controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "lists", to: 'lists#index'
-  get "lists/new", to: 'lists#new'
-  post "lists", to: 'lists#create'
-  get "lists/:id", to: 'lists#show', as: :list
+  # get 'lists/:list_id/bookmarks/new', to: 'bookmarks#new', as: :list_bookmarks
+  # post 'lists/:list_id/bookmarks', to: 'bookmarks#create', as: :create_bookmarks
+  # delete 'bookmarks/:id', to: 'bookmarks#delete'
+  # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get "lists", to: 'lists#index'
+  # get "lists/new", to: 'lists#new'
+  # post "lists", to: 'lists#create'
+  # get "lists/:id", to: 'lists#show', as: :list
 
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: :destroy
 end
